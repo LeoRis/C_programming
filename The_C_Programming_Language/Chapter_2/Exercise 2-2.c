8da8861a4a6e58c6171767d0432bfcd00b15371f
LeoRis <ristanovski@hotmail.com>
2021-01-24 20:30:43 +0100

    Update Exercise 2-2.c

--- The_C_Programming_Language/Chapter_2/Exercise 2-2.c
+++ The_C_Programming_Language/Chapter_2/Exercise 2-2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#define MAX 1000 /* maximum line size */
+#define MAX 1000 // Maximum line size
 
 int main(void) {
 
     int i, c;
-    char line[MAX]; /* current line */
+    char line[MAX]; // Current line 
 
     for(i = 0; (i < MAX - 1) * ((c=getchar()) != '\n') * (c != EOF); ++i){ // You can use "+" sign between conditions and check if all 3 conditions are == to 3.
         line[i] = c;
