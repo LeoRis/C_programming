0f2bf9a27a13daf6df6f97de59a707586fdcb44f
LeoRis <ristanovski@hotmail.com>
2021-02-05 21:28:55 +0100

    Add files via upload

---
+++ Pointers_swap_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void swap(int *n1, int *n2){
+    int temp;
+    temp = *n2;
+    *n2 = *n1;
+    *n1 = temp;
+}
+
+int main(){
+
+    int number1, number2;
+
+    printf("Insert number 1:");
+    scanf("%d", &number1);
+
+    printf("Insert number 2:");
+    scanf("%d", &number2);
+
+    swap(&number1, &number2);
+
+    printf("Number 1 is %d.\n", number1);
+    printf("Number 2 is %d.\n", number2);
+
+    return 0;
+}
