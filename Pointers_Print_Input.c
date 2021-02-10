df16496da43194d5f1b93e8d453581759f5cfda6
LeoRis <ristanovski@hotmail.com>
2021-02-10 20:55:10 +0100

    Add files via upload

---
+++ Pointers_Print_Input.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+void printArray(char *s);
+
+int main(){
+
+    char s[MAX];
+
+    printArray(s);
+
+    printf("\nThis string is %d characters long.\n", strlen(s));
+
+    return 0;
+}
+
+void printArray(char *s){
+    int i = 0;
+    char c;
+
+    while((c = getchar()) != '\n'){
+        *(s + i) = c;
+        i++;
+    }
+    *(s + i) = '\0';
+
+    printf("%s\n", s);
+}
