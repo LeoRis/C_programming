7f15bc82ac73eb768681b06858fb0c6fe51fbab6
LeoRis <ristanovski@hotmail.com>
2021-02-10 22:01:11 +0100

    Add files via upload

---
+++ Floyd_Triangle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    int rows, i, j, n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    n = 1;
+
+    for(i = 0; i < rows; i++){
+        for(j = 0; j <= i; j++){
+            printf("%d ", n);
+            n++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
