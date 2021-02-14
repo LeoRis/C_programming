4c6c944c9f383ef55463525642950c190657661c
LeoRis <ristanovski@hotmail.com>
2021-02-14 23:12:32 +0100

    Update Pyramid.c

--- Pyramid.c
+++ Pyramid.c
@@ -2,8 +2,7 @@
 
 int main(){
 
-    int rows, i, j, k;
-    char space = ' ';
+    int rows, i, j, k, l;
 
     printf("Enter number of rows: ");
     scanf("%d", &rows);
@@ -15,8 +14,34 @@ int main(){
         for(j = 0; j <= i; j++){
             printf("*");
         }
+        for(l = 1; l <= i; l++){
+            printf("*");
+        }
         printf("\n");
     }
 
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main(){
+
+    int i, j, n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for(i = 0; i <= n; i++){
+        for(j = 0; j <= 2 * n - 1; j++){
+            if(j >= n - (i - 1) && j <= n + (i - 1)){
+                printf("*");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
