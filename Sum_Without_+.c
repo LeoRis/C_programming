0e4625e6689635f22f78e883c072d25e97340194
LeoRis <ristanovski@hotmail.com>
2021-02-15 22:18:51 +0100

    Update Sum_Without_+.c

--- Sum_Without_+.c
+++ Sum_Without_+.c
@@ -27,3 +27,27 @@ int main(){
 
     return 0;
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main(){
+
+    int a, b, sum, carry;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    
+    while(b != 0){
+        sum = a ^ b;
+        carry = (a & b) << 1;
+        a = sum;
+        b = carry;
+    }
+
+    printf("The sum of the 2 numbers is: %d", sum);
+
+    return 0;
+}
