10aedfac8560dc67c4d00a82b83e0692bba536ed
LeoRis <ristanovski@hotmail.com>
2021-02-17 14:54:07 +0100

    Update Binary_To_Decimal.c

--- Binary_To_Decimal.c
+++ Binary_To_Decimal.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 
@@ -18,5 +19,32 @@ int main(){
 
     printf("Decimal equivalent of the binary number %d is %d.", number, decimal);
 
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main() {
+
+    int binary, temp, remainder, result, counter, digit;
+    remainder = result = counter = digit = 0;
+
+    printf("Enter a binary number : ");
+    scanf("%d",&binary);
+
+    temp = binary;
+
+    while(temp){
+        temp /= 10;
+        counter++;
+    }
+
+    for(int i = 0; i < counter; i++){
+        remainder = binary % 10;
+        digit = remainder * pow(2, i);
+        result += digit;
+        binary /= 10;
+    }
+    printf("%d is the required decimal number.", result);
     return 0;
 }
