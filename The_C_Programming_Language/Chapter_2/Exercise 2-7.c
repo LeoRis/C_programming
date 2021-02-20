1b8d93def9f279aad7f44cfbc80dbe6a8ef7efa6
LeoRis <ristanovski@hotmail.com>
2021-02-20 08:46:05 +0100

    Add files via upload

---
+++ The_C_Programming_Language/Chapter_2/Exercise 2-7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+unsigned invert(unsigned x, int p, int n){
+    return x ^ (~(~0U << n) << p);
+}
+
+int main(){
+  unsigned x;
+  int p, n;
+
+  for(x = 0; x < 700; x += 49){
+    for(n = 0; n < 8; n++){
+      for(p = 0; p < 8; p++){
+        printf("%x, %d, %d: %x\n", x, p, n, invert(x, p, n));
+      }
+    }
+  }
+
+  return 0;
+}
