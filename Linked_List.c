f5e141c36e569971141ec72eb592df8e5490a322
LeoRis <ristanovski@hotmail.com>
2021-02-24 21:52:57 +0100

    Update Linked_List.c

--- Linked_List.c
+++ Linked_List.c
@@ -6,6 +6,8 @@ struct node{
     struct node *link;
 };
 
+void count_of_nodes(struct node *head);
+
 int main(){
     struct node *head = malloc(sizeof(struct node));
     head->data = 45;
@@ -21,6 +23,23 @@ int main(){
     current->link = NULL;
 
     head->link->link = current;
+    
+    void count_of_nodes(struct node *head);
 
     return 0;
+}
+
+void count_of_nodes(struct node *head){
+    int count = 0;
+    if(head == NULL){
+        printf("The list is empty!");
+    }
+    struct node *ptr = NULL;
+    ptr = head;
+    
+    while(ptr != NULL){
+        count++;
+        ptr = ptr->link;
+    }
+    printf("The number of nodes is: %d", count);
 }
